0136eef7f02e2778499cf0546c8cf8509a3617d9
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2025-01-21 14:12:44 +0100

    vercel update

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,19 +4,22 @@ import dotenv from "dotenv";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15min
-  max: 100, // liczba req od IP
+  windowMs: 15 * 60 * 1000, // 15 minut
+  max: 100, // liczba żądań na IP
   message:
     "Too many requests from this IP address. Please try again in 15 minutes.",
 });
@@ -53,7 +56,6 @@ app.post("/send-email", async (req, res) => {
     });
     res.status(200).json({ message: "MESSAGE SENT" });
 
-    //zwrotka
     await transporter.sendMail({
       from: `"Marcin Ruszkowski" <${process.env.EMAIL_USER}>`,
       to: email,
@@ -74,12 +76,10 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(path.resolve(), "/dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(path.resolve(), "/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port http://localhost:${PORT}`);
-});
+export default app;
