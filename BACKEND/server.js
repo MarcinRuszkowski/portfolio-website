4ec97c26d106c13343469c4c1cb4a7199b86854d
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2025-03-16 21:28:23 +0100

    test z origin *

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
